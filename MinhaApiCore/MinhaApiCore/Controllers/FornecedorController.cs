4d1b7c58284cff07eead4a8a31c38b283acb2cf9
David <davidroliveira@gmail.com>
2021-09-23 19:52:20 -0300

    Links e Materiais

---
+++ MinhaApiCore/MinhaApiCore/Controllers/FornecedorController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinhaApiCore.Model;
+
+namespace MinhaApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedorController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public FornecedorController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Fornecedor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fornecedor>>> GetFornecedor()
+        {
+            return await _context.Fornecedor.ToListAsync();
+        }
+
+        // GET: api/Fornecedor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fornecedor>> GetFornecedor(Guid id)
+        {
+            var fornecedor = await _context.Fornecedor.FindAsync(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return fornecedor;
+        }
+
+        // PUT: api/Fornecedor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFornecedor(Guid id, Fornecedor fornecedor)
+        {
+            if (id != fornecedor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fornecedor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FornecedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Fornecedor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Fornecedor>> PostFornecedor(Fornecedor fornecedor)
+        {
+            _context.Fornecedor.Add(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFornecedor", new { id = fornecedor.Id }, fornecedor);
+        }
+
+        // DELETE: api/Fornecedor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFornecedor(Guid id)
+        {
+            var fornecedor = await _context.Fornecedor.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fornecedor.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+            return _context.Fornecedor.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MinhaApiCore/MinhaApiCore/Model/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinhaApiCore.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedor { get; set; }
+    }
+}
